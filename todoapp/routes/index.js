390020830cf6c9f3eb35de5fc40db285710580dc
yuevo <yuevo16@gmail.com>
2021-04-24 13:52:33 +0900

    ToDO追加、サーバー側作成

--- todoapp/routes/index.js
+++ todoapp/routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+let todos = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // 第一引数の「index」でejsファイルを紐付け、第二引数で渡したい値をオブジェクトで渡している
-  res.render('index', { title: 'ToDo App' });
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
+});
+
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
