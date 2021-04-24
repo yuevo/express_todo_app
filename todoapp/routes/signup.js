cf3c5053d9ec4988bf040a7e1c433c94e32be088
yuevo <yuevo16@gmail.com>
2021-04-25 00:48:37 +0900

    bcrypt導入、signupのパスワードをハッシュ化

--- todoapp/routes/signup.js
+++ todoapp/routes/signup.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   res.render('signup', {
@@ -9,6 +10,8 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -16,15 +19,16 @@ router.post('/', function (req, res, next) {
   knex("users")
     .where({name: username})
     .select("*")
-    .then(function (result) {
+    .then(async function (result) {
       if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
           errorMessage: ["このユーザ名は既に使われています"],
         }) 
       } else if (password === repassword) {
+        const hashedPassword = await bcrypt.hash(password, 10);
         knex("users")
-          .insert({name: username, password: password})
+          .insert({name: username, password: hashedPassword})
           .then(function (results) {
             req.session.userid = results[0];
             res.redirect("/");
